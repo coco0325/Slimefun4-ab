ba64627b35ee3769beef1dce28746b99e5a416d1
coco0325 <hihicoco0325@gmail.com>
2019-06-19 22:12:53 +0800

    Revert "Fixed Ancient Altar Recipes for good"
    This reverts commit 27cb4c389ca1ed8680f5642fe985621f09d2cadc.

--- src/me/mrCookieSlime/Slimefun/listeners/AncientAltarListener.java
+++ src/me/mrCookieSlime/Slimefun/listeners/AncientAltarListener.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
@@ -43,8 +42,8 @@ public class AncientAltarListener implements Listener {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 
-	List<Block> altars = new ArrayList<>();
-	Set<UUID> removed_items = new HashSet<>();
+	List<Block> altars = new ArrayList<Block>();
+	Set<UUID> removed_items = new HashSet<UUID>();
 
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onInteract(PlayerInteractEvent e) {
@@ -146,10 +145,9 @@ public class AncientAltarListener implements Listener {
 			ItemMeta im = stack.getItemMeta();
 			im.setDisplayName(null);
 			stack.setItemMeta(im);
-		} 
-		else {
+		} else {
 			ItemMeta im = stack.getItemMeta();
-			im.setDisplayName(ChatColor.RESET + customName);
+			im.setDisplayName(customName);
 			stack.setItemMeta(im);
 		}
 		return stack;
