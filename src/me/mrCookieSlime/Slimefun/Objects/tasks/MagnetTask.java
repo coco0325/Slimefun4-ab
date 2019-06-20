f19785006100348201834d98daf44b282ec3c5a9
coco0325 <hihicoco0325@gmail.com>
2019-06-20 22:28:01 +0800

    MagnetProtect

--- src/me/mrCookieSlime/Slimefun/Objects/tasks/MagnetTask.java
+++ src/me/mrCookieSlime/Slimefun/Objects/tasks/MagnetTask.java
@@ -1,5 +1,6 @@
 package me.mrCookieSlime.Slimefun.Objects.tasks;
 
+import me.mrCookieSlime.CSCoreLibPlugin.CSCoreLib;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
@@ -17,8 +18,10 @@ public class MagnetTask extends SlimefunTask {
 		for (Entity item: Bukkit.getPlayer(uuid).getNearbyEntities(6D, 6D, 6D)) {
 			if (item instanceof Item) {
 				if (!item.hasMetadata("no_pickup") && ((Item) item).getPickupDelay() <= 0) {
-					item.teleport(Bukkit.getPlayer(uuid).getEyeLocation());
-					Bukkit.getPlayer(uuid).getWorld().playSound(Bukkit.getPlayer(uuid).getEyeLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 5L, 2L);
+					if(CSCoreLib.getLib().getProtectionManager().canAccessChest(uuid, item.getLocation().getBlock())){
+						item.teleport(Bukkit.getPlayer(uuid).getEyeLocation());
+						Bukkit.getPlayer(uuid).getWorld().playSound(Bukkit.getPlayer(uuid).getEyeLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 5L, 2L);
+					}
 				}
 			}
 		}
